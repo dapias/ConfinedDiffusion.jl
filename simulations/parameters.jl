c3c8a42797dbb3d8caac3090493ed83999bbf521
dapias <dandroveros@gmail.com>
2017-05-11 14:24:51 -0500

    Initial conditions considered

--- simulations/parameters.jl
+++ simulations/parameters.jl
@@ -1,10 +1,13 @@
 nsteps = 1000
 nsampling = 1000
-nparticles = 50000
-Dx = Dy = 1.
+nparticles = 100000
+Dx = 1.0
+Dy = 1.0
 dt = 5.e-3
-lambda = 0.40
+lambda = 0.10
 sigma = 10.0
-L = 1.0
+L = 0.5
+x0 = 0.0
+y0 = 0.0
 shape = x-> sin(2*pi*x/sigma)
 
--- simulations/runsimulation.jl
+++ simulations/runsimulation.jl
@@ -3,13 +3,12 @@ include("parameters.jl")
 
 using ConfinedDiffusion
 
-filename = "prueba"
-#filename = "straight100"
-
-pos = diffusion(nparticles, nsteps, nsampling, dt, Dx, Dy, lambda, shape, L)
+#filename = "0$(y0)lambda$(lambda)sigma$(sigma)"
+filename = "Dx=$(Dx)Dy=$(Dy)-1"
+pos = diffusion(nparticles, nsteps, nsampling, dt, Dx, Dy, lambda, shape, L, x0, y0)
 
 t,D =  rms(pos, nsampling, dt)
 
-parameters = Parameters(nsteps, nsampling, nparticles, Dx, Dy, dt, lambda, sigma, L)
+parameters = Parameters(nsteps, nsampling, nparticles, Dx, Dy, dt, lambda, sigma, L, x0, y0)
 
 savedata(filename, parameters, pos, t, D)
